184ec2275029f6e7506d0aa819a76bbbc55d417c
data-lakes.io <community@data-lakes.io>
2023-06-28 19:55:14 +0200

    Add Config Example

---
+++ code/config_example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+netim = {
+    "coreApiBaseUrl": "https://192.168.5.97:8543",
+    "apiUser": "<username>",
+    "apiPassword": "<password>",
+    "validateSslCertificate": "False",
+    "metricsName":"Alluvio NetIM Power Consumption Metric"
+}
+
+meross = {
+    "apiEmail":"<email>",
+    "apiPassword":"<password>"
+}
